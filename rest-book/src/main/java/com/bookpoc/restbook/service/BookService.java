e9c3d12f83bf0e3528bb7cc3f7c3471e52ac859c
Somesh sah <somesh.sah@gmail.com>
2025-11-30 14:29:40 +0530

    added code to insert data in DB

--- rest-book/src/main/java/com/bookpoc/restbook/service/BookService.java
+++ rest-book/src/main/java/com/bookpoc/restbook/service/BookService.java
@@ -3,17 +3,29 @@ package com.bookpoc.restbook.service;
 import java.util.List;
 import java.util.Optional;
 
+import com.bookpoc.restbook.util.BookGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bookpoc.restbook.entity.Books;
 import com.bookpoc.restbook.repository.BookRepository;
 
+import javax.annotation.PostConstruct;
+
 @Service
 public class BookService {
 	
 	@Autowired
 	BookRepository bookRepository;
+
+    // while startup , it will insert 1000 records in DB
+//    @PostConstruct
+//    public void addBooksInDB(){
+//        for (int i = 1; i < 1000; i++) {
+//            Books book = BookGenerator.generateUniqueBook();
+//            bookRepository.save(book);
+//        }
+//    }
 
 	public List<Books> getAllBooks() {
 		// TODO Auto-generated method stub
--- rest-book/src/main/java/com/bookpoc/restbook/util/BookGenerator.java
+++ rest-book/src/main/java/com/bookpoc/restbook/util/BookGenerator.java
@@ -1,4 +1,32 @@
 package com.bookpoc.restbook.util;
 
+
+import com.bookpoc.restbook.entity.Books;
+
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+
+// used to generate unique records to insert in DB
 public class BookGenerator {
+
+    private static final AtomicInteger COUNTER = new AtomicInteger(1);
+    private static final Random RANDOM = new Random();
+
+    public static Books generateUniqueBook() {
+        Long id = (long) COUNTER.getAndIncrement(); // Auto-increment ID
+        String uniqueSuffix = UUID.randomUUID().toString().substring(0, 8); // Unique part
+
+        Books book = new Books();
+        book.setId(id);
+        book.setBookname("BookName_" + uniqueSuffix);
+        book.setBookauthor("Author_" + uniqueSuffix);
+        book.setPrice(RANDOM.nextInt(500) + 50);  // Generates random price between 50–550
+        book.setDiscountpercent(RANDOM.nextInt(10)); // 0–9%
+        book.setD_active(1);
+        book.setD_internalComments("Generated uniquely at " + System.currentTimeMillis());
+        book.setISBN("ISBN-" + uniqueSuffix);
+        return book;
+    }
+
 }
